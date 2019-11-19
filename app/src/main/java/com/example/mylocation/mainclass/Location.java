22229f1f0f7c29bafcb4afda3f71b7270fcc0d59
majd <majd1999ziad@gmail.com>
2019-11-19 21:25:04 +0200

    created "Location" Class

---
+++ app/src/main/java/com/example/mylocation/mainclass/Location.java
@@ -0,0 +1,59 @@
+package com.example.mylocation.mainclass;
+
+import java.util.Date;
+
+public class Location {
+    private  double latitude;
+    private  double longitude;
+    private String title;
+    private String locationDescription;
+    private Date visitingDate;
+
+    public Location(double latitude, double longitude, String title, String locationDescription, Date visitingDate) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.title = title;
+        this.locationDescription = locationDescription;
+        this.visitingDate = visitingDate;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocationDescription() {
+        return locationDescription;
+    }
+
+    public void setLocationDescription(String locationDescription) {
+        this.locationDescription = locationDescription;
+    }
+
+    public Date getVisitingDate() {
+        return visitingDate;
+    }
+
+    public void setVisitingDate(Date visitingDate) {
+        this.visitingDate = visitingDate;
+    }
+}
